a558d3b5c29615ef56ebe9e16f9c8f6be220da69
喵二 <66155308+miaoermua@users.noreply.github.com>
2025-07-17 10:06:11 +0800

    Update poweroff.lua

--- luasrc/controller/poweroff.lua
+++ luasrc/controller/poweroff.lua
@@ -16,7 +16,7 @@ module("luci.controller.poweroff", package.seeall)
 local cuci=require("luci.model.uci")
 
 function index()
-    entry({ "admin", "system", "poweroff" }, template("poweroff"), _("Poweroff"), 99).dependent = false
+    entry({ "admin", "system", "poweroff" }, template("poweroff"), _("PowerOff"), 99).dependent = false
     entry({ "admin", "system", "poweroff", "submit" }, post("action_poweroff"))
 end
 
